efff69998c3f16b468fd18652afc374e7c8a37a3
dpeabody <dpeabody@uno.edu>
2013-04-20 06:59:36 -0500

    added more picture stuff to the google map

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -21,4 +21,31 @@ class Activity < ActiveRecord::Base
   acts_as_gmappable :process_geocoding => false
 
 
+
+  def gmaps4rails_infowindow
+    # add here whatever html content you desire, it will be displayed when users clicks on the marker
+    "<img src=\"#{User.find(self.user_id).image}\"> #{self.user_id}"
+  end
+
+  def gmaps4rails_title
+    # add here whatever text you desire
+    ""
+  end
+
+  #def gmaps4rails_marker_picture
+  #  {
+  #    "picture" => ,          # string,  mandatory
+  #    "width" =>  ,          # integer, mandatory
+  #    "height" => ,          # integer, mandatory
+  #    "marker_anchor" => ,   # array,   facultative
+  #    "shadow_picture" => ,  # string,  facultative
+  #    "shadow_width" => ,    # string,  facultative
+  #    "shadow_height" => ,   # string,  facultative
+  #    "shadow_anchor" => ,   # string,  facultative
+  #    "rich_marker" =>   ,   # html, facultative
+  #    # If used, all other attributes skipped except "marker_anchor". This array is used as follows:
+  #    # [ anchor , flat ] : flat is a boolean, anchor is an int.
+  #    # See doc here: http://google-maps-utility-library-v3.googlecode.com/svn/trunk/richmarker/docs/reference.html
+  #  }
+  #end
 end
