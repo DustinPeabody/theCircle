40c32da715646848588572d8491b98099f043573
WillIam Brennan <William@WillIams-MacBook-Air-2.local>
2013-04-20 01:17:15 -0500

    fixed seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name, :type
+  attr_accessible :email, :first_name, :last_name, :user_type
   has_many :comments
   has_and_belongs_to_many :events
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user1 = User.create! :first_name => "Jorge", :last_name => "Chao", :type => "Promoter", :email => "jorge@chao.com"
+user1 = User.create! :first_name => "Jorge", :last_name => "Chao", :user_type => "Promoter", :email => "jorge@chao.com"
 location1 = Location.create! :name => "Siberia"
 event1 = Event.create! :title => "The Ghostwood", :location => location1
