5f6b2fa74eb9828687d5ee4ae5edaa5736ee0722
Charlemagne3 <blackbeltcharles@gmail.com>
2013-04-20 01:54:21 -0500

    added column change migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130420045041) do
+ActiveRecord::Schema.define(:version => 20130420060914) do
 
   create_table "activities", :force => true do |t|
     t.integer  "user_id"
@@ -62,11 +62,11 @@ ActiveRecord::Schema.define(:version => 20130420045041) do
   end
 
   create_table "tasks", :force => true do |t|
-    t.string   "type"
     t.integer  "event_id"
     t.boolean  "completed"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "task_type"
   end
 
   create_table "user_rsvps", :force => true do |t|
