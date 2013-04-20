58b6900ae646aae9799e61b19fd818e2c779c989
Charlemagne3 <blackbeltcharles@gmail.com>
2013-04-20 07:58:34 -0500

    fixed naming error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ TheCircle::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  Paperclip.options[:command_path] = "/Users/petrobook_pro/.rvm/bin/"
 end
