46c38fb41d073f727473250bfe3d90acc10fd96e
dpeabody <dpeabody@uno.edu>
2013-04-19 21:21:08 -0500

    Base app here ya go

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheCircle::Application.config.secret_token = 'a0e3875ce32105e2a3303ab4153a65c94bfe3738f3d5417542ceac6174daf7071f5b147ce6184583c365aa7ce76105363f57ea84d2c37bd4e59df9c8c6339ab0'
